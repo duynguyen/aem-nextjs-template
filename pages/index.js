7d6a977b0228b3aa35491f3d913c1b9b5375cd75
Duy Nguyen <dunguyen@adobe.com>
2022-05-05 01:21:05 +0200

    use fetchModel from react SDK

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
-import { getPageModel, getItemFromPageModel, modelToProps } from '../lib/pages';
-import { ResponsiveGrid } from '@adobe/aem-react-editable-components'
+import { ResponsiveGrid, fetchModel } from '@adobe/aem-react-editable-components'
 
 const { NEXT_PUBLIC_AEM_PATH } = process.env;
 
 export default function Home({ model }) {
-  const responsiveGridModel = getItemFromPageModel(model, 'root/responsivegrid');
-  const responsiveGridModelProps = modelToProps(responsiveGridModel);
   return (
     <Layout>
       <Head>
@@ -16,7 +13,7 @@ export default function Home({ model }) {
       <section>
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-4 sm:py-2 lg:py-6">
           <ResponsiveGrid
-            model = {responsiveGridModelProps}
+            model = {model}
             pagePath={NEXT_PUBLIC_AEM_PATH}
             itemPath='root/responsivegrid'
           />
@@ -27,7 +24,7 @@ export default function Home({ model }) {
 }
 
 export async function getServerSideProps() {
-  const model = await getPageModel(NEXT_PUBLIC_AEM_PATH);
+  const model = await fetchModel({pagePath: NEXT_PUBLIC_AEM_PATH, itemPath: 'root/responsivegrid'});
   return {
     props: {
       model
